fa5933c18f9232c12d347edcb90508d1cd250134
webshakil <chumkysk@gmail.com>
2022-03-13 16:11:21 +0600

    typea of primative data

---
+++ JavascriptOOP/Oop/oop.js
@@ -0,0 +1,37 @@
+var color1="red";
+var color2 = color1;
+// console.log(color1,color2);
+
+var color1="green"
+
+console.log(color1);
+
+console.log(color2);
+
+console.log(typeof null);
+
+console.log("5"==5);
+console.log("5"===5);
+
+console.log(value===null);
+console.log(undefined==null);
+
+console.log(undefined===null);
+
+var namep="Nickolas";
+
+var Lowercase =namep.toLowerCase();
+
+console.log(namep);
+
+var firstLetter = namep.charAt(0);
+console.log(firstLetter);
+
+// var name = "Nicholas";
+// var lowercaseName = name.toLowerCase(); 
+
+// console.log(lowercaseName);
+
+
+
+
